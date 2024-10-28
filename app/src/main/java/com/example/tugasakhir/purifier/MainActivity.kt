e267c018fb0960686117b2746597a6adb2879107
sutanbaihaqi23 <116020557+sutanbaihaqi23@users.noreply.github.com>
2024-10-28 21:06:44 +0700

    fixed main activity

--- app/src/main/java/com/example/tugasakhir/purifier/MainActivity.kt
+++ app/src/main/java/com/example/tugasakhir/purifier/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.tugasakhir.purifier
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.tugasakhir.purifier.databinding.ActivityMainBinding
+import com.example.tugasakhir.purifier.databinding.DialogRecycleviewBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val rvForecast: RecyclerView = findViewById(R.id.rvForecast)
         val forecastItems = listOf(
@@ -18,6 +24,11 @@ class MainActivity : AppCompatActivity() {
             ForecastItem("0-50", R.drawable.sun,  R.drawable.wind, 27,"1.2km/h", "13.00"),
             ForecastItem("0-50", R.drawable.sun,  R.drawable.wind, 27,"1.2km/h", "14.00")
         )
+
+        binding.iconInfo.setOnClickListener {
+
+        }
+
 
         val adapter = ForecastAdapter(forecastItems)
         rvForecast.adapter = adapter
